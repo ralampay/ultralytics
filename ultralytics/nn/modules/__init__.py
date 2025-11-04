80037d7065f21dd9c9223e35b8b75cc9df42321f
Raphael Alampay <raphael.alampay@gmail.com>
2025-11-05 01:20:38 +0800

    initial CoordAttnConv custom block

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -18,7 +18,7 @@ Examples:
 """
 
 from .custom_blocks import (
-    MyConvBlock
+    CoordAttConv
 )
 
 from .block import (
@@ -115,7 +115,7 @@ __all__ = (
     "AIFI",
     "Conv",
     "Conv2",
-    "MyConvBlock",
+    "CoordAttConv",
     "LightConv",
     "RepConv",
     "DWConv",
